4055c4a560df9e7fea0e4c9d6b39830cb33c0268
Derrick Beining <derrick.beining@gmail.com>
2017-08-30 18:15:35 -0400

    upgraded to latest webpack, managed to update config, but client console is full of errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,49 @@
 'use strict';
+const path = require('path')
 
-var webpack = require('webpack');
+const webpack = require('webpack');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './browser/main.jsx',
   output: {
-    path: __dirname,
-    filename: './public/bundle.js'
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   context: __dirname,
   devtool: 'source-map',
   resolve: {
-    extensions: [ '', '.js', '.jsx' ]
+    modules: [
+      'node_modules',
+
+    ],
+    extensions: [ '.js', '.jsx' ]
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'browser')
+        ],
+        loader: 'babel-loader',
+        options: {
           presets: [ 'react', 'es2015', 'stage-1', 'stage-2', 'stage-3' ]
-        }
-      }
-    ]
-  }
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: false,
+      },
+      parallel: {
+        cache: true,
+      },
+      sourceMap: true
+    }),
+  ]
 };
